1b311f76b9bf477ef7e67510a2a63b20ae2bcf39
Lucian Sirbu <quamis@gmail.com>
2012-11-22 01:31:33 +0200

    NEW: show more stats

--- myHouse/stats.py
+++ myHouse/stats.py
@@ -18,7 +18,7 @@ import numpy
 class Stats:
 	def __init__(self, db):
 		self.db = db
-		
+	
 	def extract(self, args):
 		#              0       1           2          3       4          5   
 		sql = "SELECT `id`, `category`, `source`, `price`, `addDate`, `updateDate`  FROM `data` WHERE 1 ORDER BY `price` ASC, `description` ASC"
@@ -52,9 +52,15 @@ class Stats:
 			timeSinceAppeared.append(timestamp - int(row[4]))
 			timeSinceDisappeared.append(timestamp - int(row[5]))
 
-		stats['price_per_category'] = {}			
+		stats['price_per_category:mean'] = {}
+		stats['price_per_category:median'] = {}
+		stats['price_per_category:std'] = {}
+		stats['price_per_category:var'] = {}
 		for categ in prices:
-			stats['price_per_category'][categ] = numpy.mean(prices[categ])
+			stats['price_per_category:median'][categ] = numpy.median(prices[categ])
+			stats['price_per_category:mean'][categ] = numpy.mean(prices[categ])
+			stats['price_per_category:std'][categ] = numpy.std(prices[categ])
+			stats['price_per_category:var'][categ] = numpy.var(prices[categ])
 		
 		stats['alivePeriod'] = 			numpy.mean(alivePeriods)/(60*60*24)
 		stats['timeSinceAppeared'] = 	numpy.mean(timeSinceAppeared)/(60*60*24)
@@ -62,6 +68,22 @@ class Stats:
 		
 		self.db.selectEnd(rows)
 		print stats
+		self.printStats(stats)
+		
+	def printStats(self, stats):
+		sys.stdout.write("\n Sources: ")
+		s = ""
+		for src in stats['source']:
+			sys.stdout.write("%s %s(%s)" % (s, src, locale.format("%.*f", (0, stats['source'][src]), True)))
+			s = ","
+			
+		sys.stdout.write("\n Categories: ")
+		s = ""
+		for src in stats['categories']:
+			sys.stdout.write("\n\t%s: \t% 5s \tmedia % 7s EUR (% 7s EUR)" % (src, 
+				locale.format("%.*f", (0, stats['categories'][src]), True), 
+				locale.format("%.*f", (0, stats['price_per_category:mean'][src]), True),
+				locale.format("%.*f", (0, stats['price_per_category:median'][src]), True)))
 	
 parser = argparse.ArgumentParser(description='Filter gatherer results.')
 parser.add_argument('-minPrice', 	dest='minPrice', 	action='store', 	type=int, default=30000,	help='min price to match')
@@ -76,5 +98,5 @@ sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 db = DB("main.sqlite")
 stats = Stats(db)
 stats.extract(args)
-
+sys.stdout.write("\n")
 raise SystemExit
