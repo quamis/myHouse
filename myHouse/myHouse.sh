d80ff9524394e745b448244e002802271082600d
Lucian Sirbu <quamis@gmail.com>
2012-12-29 23:41:19 +0200

    NEW: a simpler updater script, symlinked to the home folder

---
+++ myHouse/myHouse.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd git/myHouse/myHouse/
+./update.sh
+
+OUT="/tmp/myHouse.log"
+echo "case-noi-metrou" > $OUT
+./view.py --profile=case-noi-metrou >> $OUT
+echo "" >> $OUT
+echo "" >> $OUT
+echo "" >> $OUT
+echo "case-noi" >> $OUT
+./view.py --profile=case-noi >> $OUT
+
+
+#/view.py --profile=none -age=9999 -agea=9999 -status=todo
+#notify/notify.py -user="rassandra21@gmail.com" -message="`cat /tmp/myHouse.log`"
+
+cat $OUT
+
+rm $OUT
