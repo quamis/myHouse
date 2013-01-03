cad7dc57588677e2d8986e1c742840ffe536923f
Lucian Sirbu <quamis@gmail.com>
2013-01-03 19:50:30 +0200

    FIX: pipe the output through "less"

--- myHouse/myHouse.sh
+++ myHouse/myHouse.sh
@@ -16,6 +16,6 @@ echo "case-noi" >> $OUT
 #/view.py --profile=none -age=9999 -agea=9999 -status=todo
 #notify/notify.py -user="rassandra21@gmail.com" -message="`cat /tmp/myHouse.log`"
 
-cat $OUT
+cat $OUT | less
 
 rm $OUT
