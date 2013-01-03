8e2c67a5f4203fb014ef9cae27954470d1de8bff
Lucian Sirbu <quamis@gmail.com>
2013-01-03 19:51:43 +0200

    NEW: detele items indexed by day (links) NEW: randomly vacuum the DB FIX: close the db(to make sure the changes are commited)

--- myHouse/base/cleanup.py
+++ myHouse/base/cleanup.py
@@ -2,6 +2,7 @@ import time
 import sys
 from datetime import date, timedelta
 import difflib
+import random
 
 
 class Cleanup(object):
@@ -13,18 +14,26 @@ class Cleanup(object):
         self.tables = []
         
     def cleanCache(self):
-        dt = date.today()-timedelta(days=3)
-        maxUpdatedTime = time.mktime(dt.timetuple())
+        maxUpdatedTime = time.mktime((date.today()-timedelta(days=2)).timetuple())
 
         for table in self.tables:
             count1 = self.cache.db.selectAll("SELECT COUNT(*) FROM `%s`" % (table))[0][0]
             self.cache.db.execute("DELETE FROM `%s` WHERE `addDate`<%d OR `addDate` IS NULL" % (table, maxUpdatedTime))
+            
+            self.cache.db.execute("DELETE FROM `%s` WHERE ( `id` NOT LIKE 'page-%%' AND `id` NOT LIKE '%s%%-%%' )" % (table, time.strftime("%Y%m%d")))
+            
             count2 = self.cache.db.selectAll("SELECT COUNT(*) FROM `%s`" % (table))[0][0]
             
             print "%s: %s -> %s" % (table, count1, count2)
+        
+        self.vacuum()
             
-        #self.cache.db.execute("VACUUM")
+    def vacuum(self):
+        if random.random()<0.20:
+            self.cache.db.execute("VACUUM")
 
     def run(self):
         self.cleanCache()
+        
+        self.cache.db.close()
         pass
