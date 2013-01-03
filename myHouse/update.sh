a82fcef69ad9ba0841d58920161daffa504826ac
Lucian Sirbu <quamis@gmail.com>
2013-01-04 00:21:06 +0200

    NEW: the sources list in now an bash array, and we loop over it to gather, process, cleanup them FIX: lots of small changes, and made everything more configurable

--- myHouse/update.sh
+++ myHouse/update.sh
@@ -19,85 +19,87 @@ function do_unlock(){
     rm "$LDIR/$SYS"
 }
 
-LDIR="../db/locks/"
-
-OFILE="/dev/null"
-
-DT=`date +"%Y-%m-%d %H:%M:%S"`
-echo "[$DT] updater started"
-
-
 function gather_anuntul_ro(){
+    local SLEEP="0.75"
+    local V=5
     do_lock "anuntul_ro" "case-vile"
-    ./gather.py -v=1 -sleep=1.0 -user-agent="random" -module "anuntul_ro" -category "case-vile" -url "http://www.anuntul.ro/anunturi-imobiliare-vanzari/case-vile/pag-1/"               
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "anuntul_ro" -category "case-vile" -url "http://www.anuntul.ro/anunturi-imobiliare-vanzari/case-vile/pag-1/"               
     
     do_lock "anuntul_ro" "apt-2-cam"
-    ./gather.py -v=1 -sleep=0.5 -user-agent="random" -module "anuntul_ro" -category "apt-2-cam" -url "http://www.anuntul.ro/anunturi-imobiliare-vanzari/apartamente-2-camere/pag-1/"    
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "anuntul_ro" -category "apt-2-cam" -url "http://www.anuntul.ro/anunturi-imobiliare-vanzari/apartamente-2-camere/pag-1/"    
     
     do_lock "anuntul_ro" "apt-3-cam"
-    ./gather.py -v=1 -sleep=0.5 -user-agent="random" -module "anuntul_ro" -category "apt-3-cam" -url "http://www.anuntul.ro/anunturi-imobiliare-vanzari/apartamente-3-camere/pag-1/"     
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "anuntul_ro" -category "apt-3-cam" -url "http://www.anuntul.ro/anunturi-imobiliare-vanzari/apartamente-3-camere/pag-1/"     
     
     do_lock "anuntul_ro" "apt-4-cam"
-    ./gather.py -v=1 -sleep=0.5 -user-agent="random" -module "anuntul_ro" -category "apt-4-cam" -url "http://www.anuntul.ro/anunturi-imobiliare-vanzari/apartamente-4-camere/pag-1/"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "anuntul_ro" -category "apt-4-cam" -url "http://www.anuntul.ro/anunturi-imobiliare-vanzari/apartamente-4-camere/pag-1/"
     
     do_unlock "anuntul_ro"
 }
 
 
 function gather_imobiliare_ro(){
+    local SLEEP="0.5"
+    local V=5
     do_lock "imobiliare_ro" "case-vile"
-    ./gather.py -v=1 -sleep=0.5 -user-agent="real" -module "imobiliare_ro" -category "case-vile" -url "http://www.imobiliare.ro/vanzare-case-vile/bucuresti"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="real" -module "imobiliare_ro" -category "case-vile" -url "http://www.imobiliare.ro/vanzare-case-vile/bucuresti"
     
     do_lock "imobiliare_ro" "apt-2-cam"
-    ./gather.py -v=1 -sleep=0.5 -user-agent="real" -module "imobiliare_ro" -category "apt-2-cam" -url "http://www.imobiliare.ro/vanzare-apartamente/bucuresti?nrcamere=2"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="real" -module "imobiliare_ro" -category "apt-2-cam" -url "http://www.imobiliare.ro/vanzare-apartamente/bucuresti?nrcamere=2"
     
     do_lock "imobiliare_ro" "apt-3-cam"
-    ./gather.py -v=1 -sleep=0.5 -user-agent="real" -module "imobiliare_ro" -category "apt-3-cam" -url "http://www.imobiliare.ro/vanzare-apartamente/bucuresti?nrcamere=3"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="real" -module "imobiliare_ro" -category "apt-3-cam" -url "http://www.imobiliare.ro/vanzare-apartamente/bucuresti?nrcamere=3"
     
     do_lock "imobiliare_ro" "apt-2-cam"
-    ./gather.py -v=1 -sleep=0.5 -user-agent="real" -module "imobiliare_ro" -category "apt-4-cam" -url "http://www.imobiliare.ro/vanzare-apartamente/bucuresti?nrcamere=4"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="real" -module "imobiliare_ro" -category "apt-4-cam" -url "http://www.imobiliare.ro/vanzare-apartamente/bucuresti?nrcamere=4"
     
     do_unlock "imobiliare_ro"
 }
 
 
 function gather_tocmai_ro(){
+    local SLEEP="0.75"
+    local V=5
     do_lock "tocmai_ro" "case-vile"
-    ./gather.py -v=1 -sleep=0.0 -user-agent="random" -module "tocmai_ro" -category "case-vile" -url "http://www.tocmai.ro/cauta?page=1&typ=1&ct=8&jd=26&tz=1"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "tocmai_ro" -category "case-vile" -url "http://www.tocmai.ro/cauta?page=1&typ=1&ct=8&jd=26&tz=1"
     
     do_lock "tocmai_ro" "apt-2-cam"
-    ./gather.py -v=1 -sleep=0.0 -user-agent="random" -module "tocmai_ro" -category "apt-2-cam" -url "http://www.tocmai.ro/cauta?typ=1&ct=6&jd=26&tz=1&cm=2&img=1"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "tocmai_ro" -category "apt-2-cam" -url "http://www.tocmai.ro/cauta?typ=1&ct=6&jd=26&tz=1&cm=2&img=1"
     
     do_lock "tocmai_ro" "apt-2-cam"
-    ./gather.py -v=1 -sleep=0.0 -user-agent="random" -module "tocmai_ro" -category "apt-3-cam" -url "http://www.tocmai.ro/cauta?typ=1&ct=6&jd=26&tz=1&cm=3&img=1"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "tocmai_ro" -category "apt-3-cam" -url "http://www.tocmai.ro/cauta?typ=1&ct=6&jd=26&tz=1&cm=3&img=1"
     
     do_lock "tocmai_ro" "apt-2-cam"
-    ./gather.py -v=1 -sleep=0.0 -user-agent="random" -module "tocmai_ro" -category "apt-4-cam" -url "http://www.tocmai.ro/cauta?typ=1&ct=6&jd=26&tz=1&cm=4&img=1"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "tocmai_ro" -category "apt-4-cam" -url "http://www.tocmai.ro/cauta?typ=1&ct=6&jd=26&tz=1&cm=4&img=1"
     
     do_unlock "tocmai_ro"
 }
 
 
 function gather_mercador_ro(){
+    local SLEEP="0.75"
+    local V=5
     do_lock "mercador_ro" "case-vile(limited-search)"
-    ./gather.py -v=1 -sleep=0.75 -user-agent="random" -module "mercador_ro" -category "case-vile" -url "http://mercador.ro/imobiliare/case-de-vanzare-si-de-inchiriat/bucuresti/?search[filter_float_price%3Afrom]=20000&search[filter_float_price%3Ato]=90000&search[filter_enum_alege]=vanzare"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "mercador_ro" -category "case-vile" -url "http://mercador.ro/imobiliare/case-de-vanzare-si-de-inchiriat/bucuresti/?search[filter_float_price%3Afrom]=20000&search[filter_float_price%3Ato]=90000&search[filter_enum_alege]=vanzare"
     
     do_unlock "mercador_ro"
 }
 
 
 function gather_imopedia_ro(){
+    local SLEEP="1.5"
+    local V=5
     do_lock "imopedia_ro" "case-vile"
-    ./gather.py -v=1 -sleep=1.5 -user-agent="random" -module "imopedia_ro" -category "case-vile" -url "http://www.imopedia.ro/bucuresti/vanzari-vile.html"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "imopedia_ro" -category "case-vile" -url "http://www.imopedia.ro/bucuresti/vanzari-vile.html"
     
     do_lock "imopedia_ro" "apt-2-cam"
-    ./gather.py -v=1 -sleep=1.5 -user-agent="random" -module "imopedia_ro" -category "apt-2-cam" -url "http://www.imopedia.ro/bucuresti/apartamente-de-vanzare-2-camere-1104056-0-pagina-0.html"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "imopedia_ro" -category "apt-2-cam" -url "http://www.imopedia.ro/bucuresti/apartamente-de-vanzare-2-camere-1104056-0-pagina-0.html"
     
     do_lock "imopedia_ro" "apt-3-cam"
-    ./gather.py -v=1 -sleep=1.5 -user-agent="random" -module "imopedia_ro" -category "apt-3-cam" -url "http://www.imopedia.ro/bucuresti/apartamente-de-vanzare-3-camere-1102874-0-pagina-0.html"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "imopedia_ro" -category "apt-3-cam" -url "http://www.imopedia.ro/bucuresti/apartamente-de-vanzare-3-camere-1102874-0-pagina-0.html"
     
     do_lock "imopedia_ro" "apt-4-cam"
-    ./gather.py -v=1 -sleep=1.5 -user-agent="random" -module "imopedia_ro" -category "apt-4-cam" -url "http://www.imopedia.ro/bucuresti/apartamente-de-vanzare-4-camere-1104405-0-pagina-0.html"
+    ./gather.py -v=$V -sleep=$SLEEP -user-agent="random" -module "imopedia_ro" -category "apt-4-cam" -url "http://www.imopedia.ro/bucuresti/apartamente-de-vanzare-4-camere-1104405-0-pagina-0.html"
     
     do_unlock "imopedia_ro"
 }
@@ -145,27 +147,47 @@ while test $# -gt 0; do
     shift
 done
 
+
+################################################################################
+LDIR="../db/locks/"
+OFILE="/tmp/myHouse-update.sh-"
+DT=`date +"%Y-%m-%d %H:%M:%S"`
+echo "[$DT] updater started"
+
+
+SOURCES=( "anuntul_ro" "imobiliare_ro" "tocmai_ro" "mercador_ro" "imopedia_ro" "az_ro" )
+#SOURCES=( "anuntul_ro" )
+#SOURCES=( "imopedia_ro" )
+
+
 if [ "$GATHER" == "default" -o "$GATHER" == "linear" ]; then
     # Starting background processes
     if [ "$GATHER" == "linear" ]; then
-        echo "Starting linear processes"
-        gather_anuntul_ro
-        gather_imobiliare_ro
-        gather_tocmai_ro
-        gather_mercador_ro
-        gather_imopedia_ro
-        gather_az_ro
+        printf "[`date +"%Y-%m-%d %H:%M:%S"`] Starting linear processes: "
+        S=""
+        for src in "${SOURCES[@]}";do
+            printf "%s%s" "$S" "$src"
+            S=", "
+
+            # do the actual call
+            gather_${src}
+        done
+        printf "\n"
     else
-        echo "Starting background processes"
-        gather_anuntul_ro 1>&2 >"$OFILE" &
-        gather_imobiliare_ro 1>&2 >"$OFILE" &
-        gather_tocmai_ro 1>&2 >"$OFILE" &
-        gather_mercador_ro 1>&2 >"$OFILE" &
-        gather_imopedia_ro 1>&2 >"$OFILE" &
-        gather_az_ro 1>&2 >"$OFILE" &
+        printf "[`date +"%Y-%m-%d %H:%M:%S"`] Starting background processes: "
+        S=""
+        for src in "${SOURCES[@]}";do
+            printf "%s%s" "$S" "$src"
+            S=", "
+            
+            # do the actual call
+            gather_${src} >"${OFILE}${src}" 2>&1 &
+        done
+        printf "\n"
     fi;
     
     
+    # the number of existing lockfiles
     LOCKSC=99999
     while [ $LOCKSC -gt 0 ]; do
         LOCKS=`ls -1 "$LDIR" | sort`
@@ -191,27 +213,29 @@ fi;
 
 
 if [ "$PROCESS" == "default" ]; then
-    ./process.py -v=1 -module "anuntul_ro"
-    ./process.py -v=1 -module "imobiliare_ro"
-    ./process.py -v=1 -module "tocmai_ro"
-    ./process.py -v=1 -module "az_ro"
-    ./process.py -v=1 -module "mercador_ro"
-    ./process.py -v=1 -module "imopedia_ro"
+    for src in "${SOURCES[@]}";do
+        printf "[`date +"%Y-%m-%d %H:%M:%S"`] Processing: %s\n" "$src"
+        
+        # do the actual call
+        ./process.py -v=1 -module="${src}"
+    done
+    printf "\n"
 fi;
 
 
 
 if [ "$CLEANUP" == "default" ]; then
-    echo "cleanup";
+    printf "[`date +"%Y-%m-%d %H:%M:%S"`] Cleanup\n"
     #./cleanup.py -v=5 -vacuum=1
     ./cleanup.py -v=1 -nodescription=1 -fixstatus=1 
-    #./cleanup.py -v=5 -deleteOldItems=1 
+    ./cleanup.py -v=5 -deleteOldItems=1 
     
-    ./cleanup.py -module="anuntul_ro"
-    ./cleanup.py -module="imobiliare_ro"
-    ./cleanup.py -module="tocmai_ro"
-    ./cleanup.py -module="az_ro"
-    ./cleanup.py -module="mercador_ro"
-    ./cleanup.py -module="imopedia_ro"
+    for src in "${SOURCES[@]}";do
+        printf "[`date +"%Y-%m-%d %H:%M:%S"`] Cleanup: %s\n" "$src"
+        
+        # do the actual call
+        ./cleanup.py -module="${src}"
+    done
+    printf "\n"
 fi
 
