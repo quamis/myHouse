3f9ba6cb476e0922c7864b0fda232d683d235437
Lucian Sirbu <quamis@gmail.com>
2013-01-05 02:35:35 +0200

    NEW: the script is now able to send text emails

--- myHouse/notify/notify.py
+++ myHouse/notify/notify.py
@@ -6,6 +6,7 @@ import argparse
 # TODO: remove this
 # ./notify.py -user="rassandra21@gmail.com" -message="te iubesc"
 
+"""
 parser = argparse.ArgumentParser(description='Send notifications')
 parser.add_argument('-user',        dest='user',     action='store', type=str, default=None,    help='TODO')
 parser.add_argument('-message',     dest='message',  action='store', type=str, default=None,    help='TODO')
@@ -28,5 +29,30 @@ purple.PurpleConvImSend(im, args.message)
 
 time.sleep(10)
 purple.PurpleCoreQuit()
+"""
 
 
+import smtplib
+from email.mime.text import MIMEText
+parser = argparse.ArgumentParser(description='Send notifications')
+parser.add_argument('-sender',      dest='sender',   action='store', type=str, default=None,    help='TODO')
+parser.add_argument('-password',    dest='password', action='store', type=str, default=None,    help='TODO')
+parser.add_argument('-to',          dest='to',       action='store', type=str, default=None,    help='TODO')
+parser.add_argument('-message',     dest='message',  action='store', type=str, default=None,    help='TODO')
+args = parser.parse_args()
+
+print "Sending email to %s" % ( args.to )
+
+# @see http://docs.python.org/2/library/email-examples.html#email-examples for a html message example
+
+messageBody = MIMEText(args.message)
+messageBody['Subject'] = 'myHouse script results'
+messageBody['From'] = args.sender
+messageBody['To'] = args.to 
+
+server = smtplib.SMTP('smtp.gmail.com:587')  
+server.starttls()  
+server.login(args.sender, args.password)  
+server.sendmail(args.sender, args.to, messageBody.as_string())  
+server.quit()  
+print "Email sent"
