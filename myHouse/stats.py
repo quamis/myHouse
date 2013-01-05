f256cd655b263866d9fb39c55d79c1a6cdf14a92
Lucian Sirbu <quamis@gmail.com>
2013-01-06 01:40:17 +0200

    NEW: added some date-based statistics NEW: added the possibility to filter by system, status, category

--- myHouse/stats.py
+++ myHouse/stats.py
@@ -6,7 +6,7 @@
 
 import sys, time, os, codecs
 import re
-from datetime import date, timedelta
+import datetime
 
 from DB import DB
 from CACHE import CACHE
@@ -16,14 +16,34 @@ import argparse
 import numpy 
 
 class Stats:
-	def __init__(self, db):
+	def __init__(self, db, args):
 		self.db = db
+		self.args = args
 	
-	def extract(self, args):
+	
+	def getSQL(self):
 		#              0     1           2         3        4          5             6         7
-		sql = "SELECT `id`, `category`, `source`, `price`, `addDate`, `updateDate`, `status`, `description`  FROM `data` WHERE 1" \
-			+ " ORDER BY `price` ASC, `description` ASC"
+		sql = "SELECT `id`, `category`, `source`, `price`, `addDate`, `updateDate`, `status`, `description`  FROM `data` WHERE 1"
 		
+		if self.args.category:
+			sql+="/*category*/ AND( 0 "
+			for k in self.args.category:
+				sql+=" OR `category`='%s'" % (k)
+			sql+=")"
+			
+		if self.args.source:
+			sql+="/*source*/ AND( 0 "
+			for k in self.args.source:
+				sql+=" OR `source`='%s'" % (k)
+			sql+=")"
+			
+		if self.args.status:
+			sql+="/*status*/ AND (`status`='%s')" % (self.args.status)
+		
+		sql += " ORDER BY `price` ASC, `description` ASC"
+		return sql
+	
+	def extractGeneralData(self):
 		timestamp = time.time()
 		
 		stats = {}
@@ -36,7 +56,7 @@ class Stats:
 		timeSinceAppeared = []
 		timeSinceDisappeared = []
 		
-		rows = self.db.selectStart(sql)
+		rows = self.db.selectStart(self.getSQL())
 		for row in rows:
 			if row[1] not in stats['categories']:
 				stats['categories'][row[1]] = 0
@@ -75,7 +95,7 @@ class Stats:
 		self.db.selectEnd(rows)
 		return stats
 		
-	def printStats(self, stats):
+	def printGeneralData(self, stats):
 		sys.stdout.write("\n\n Sources: ")
 		s = ""
 		for src in stats['source']:
@@ -118,12 +138,93 @@ class Stats:
 			if src in stats['statuses']:
 				sys.stdout.write("\n\t%- 9s: % 9s" % (src, locale.format("%.*f", (0, stats['statuses'][src]), True)))
 		
-		
-		
 		sys.stdout.write("\n\n Times: %d days alive, appeared %d days ago, dissapeared %d days ago" % (stats['alivePeriod'], stats['timeSinceAppeared'], stats['timeSinceDisappeared']))
-	
+
+	def _incDict(self, dict, key):
+		if key not in dict:
+			dict[key] = 0
+			
+		dict[key] +=1
+		
+		return dict 
+
+	def extractDateData(self):
+		stats = {}
+		stats['addDate'] = {}
+		stats['updateDate'] = {}
+		
+		if self.args.byDate=="week":
+			stats['addDate:week'] = {}
+			stats['updateDate:week'] = {}
+			
+		if self.args.byDate=="month":
+			stats['addDate:month'] = {}
+			stats['updateDate:month'] = {}
+		
+		rows = self.db.selectStart(self.getSQL())
+		for row in rows:
+			addDate = datetime.datetime.fromtimestamp(row[4]).strftime('%Y-%m-%d')
+			updateDate = datetime.datetime.fromtimestamp(row[5]).strftime('%Y-%m-%d')
+			
+			stats['addDate'] = 		self._incDict(stats['addDate'], addDate)
+			stats['updateDate'] = 	self._incDict(stats['updateDate'], updateDate)
+			
+			if self.args.byDate=="week":
+				addDate = datetime.datetime.fromtimestamp(row[4]).strftime('%Y-%U')
+				updateDate = datetime.datetime.fromtimestamp(row[5]).strftime('%Y-%U')
+				
+				stats['addDate:week'] = 	self._incDict(stats['addDate:week'], addDate)
+				stats['updateDate:week'] = 	self._incDict(stats['updateDate:week'], updateDate)
+				
+			if self.args.byDate=="month":
+				addDate = datetime.datetime.fromtimestamp(row[4]).strftime('%Y-%m')
+				updateDate = datetime.datetime.fromtimestamp(row[5]).strftime('%Y-%m')
+				
+				stats['addDate:month'] = 		self._incDict(stats['addDate:month'], addDate)
+				stats['updateDate:month'] = 	self._incDict(stats['updateDate:month'], updateDate)
+			
+		return stats
+		
+	def printDateData(self, stats):
+		
+		if self.args.byDate=="day":
+			allKeys = list(set( stats['addDate'].keys() + stats['updateDate'].keys() ))
+			fmt = "%-12s: % 9s % 9s"
+			print (fmt+" offers") % ("date", "added", "updated")
+						
+			for key in sorted(allKeys):
+				print fmt % (key, 
+					locale.format("%.*f", (0, stats['addDate'][key]), True) 	if key in stats['addDate'] 		else 0, 
+					locale.format("%.*f", (0, stats['updateDate'][key]), True) 	if key in stats['updateDate'] 	else 0,)
+				
+		elif self.args.byDate=="week":
+			allKeys = list(set( stats['addDate:week'].keys() + stats['updateDate:week'].keys() ))
+			fmt = "%-12s: % 9s % 9s"
+			print (fmt+" offers") % ("date", "added", "updated")
+						
+			for key in sorted(allKeys):
+				print fmt % (key, 
+					locale.format("%.*f", (0, stats['addDate:week'][key]), True) 		if key in stats['addDate:week'] 	else 0, 
+					locale.format("%.*f", (0, stats['updateDate:week'][key]), True) 	if key in stats['updateDate:week'] 	else 0,)
+				
+		elif self.args.byDate=="month":
+			allKeys = list(set( stats['addDate:month'].keys() + stats['updateDate:month'].keys() ))
+			fmt = "%-12s: % 9s % 9s"
+			print (fmt+" offers") % ("date", "added", "updated")
+						
+			for key in sorted(allKeys):
+				print fmt % (key, 
+					locale.format("%.*f", (0, stats['addDate:month'][key]), True) 		if key in stats['addDate:month'] 	else 0, 
+					locale.format("%.*f", (0, stats['updateDate:month'][key]), True) 	if key in stats['updateDate:month'] else 0,)
+		
+		
+			
 parser = argparse.ArgumentParser(description='Filter gatherer results.')
-parser.add_argument('-byDate', 	dest='byDate', 	action='store', 	type=str, default="day",	help='')
+parser.add_argument('-source', 	dest='source', 	action='append', 	type=str, default=None,	help='TODO')
+parser.add_argument('-category',dest='category',action='append', 	type=str, default=None,	help='TODO')
+parser.add_argument('-status',	dest='status',	action='store', 	type=str, default=None,	help='TODO')
+
+parser.add_argument('-byDate', 	dest='byDate', 	action='store', 	type=str, default=None,	help='TODO')
 args = parser.parse_args()
 
 logging.basicConfig(format='%(asctime)s %(message)s',level=logging.DEBUG)
@@ -133,7 +234,12 @@ locale.setlocale(locale.LC_NUMERIC, '')
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
 db = DB("../db/main.sqlite")
-stats = Stats(db)
-stats.printStats(stats.extract(args))
+stats = Stats(db, args)
+
+if args.byDate:
+	stats.printDateData(stats.extractDateData())
+else:
+	stats.printGeneralData(stats.extractGeneralData())
+	
 sys.stdout.write("\n")
 raise SystemExit
