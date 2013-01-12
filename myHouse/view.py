94f1ae9bbf9b987784099b6f2bdd4028f57855b1
Lucian Sirbu <quamis@gmail.com>
2013-01-12 14:37:30 +0200

    FIX: code cleanup

--- myHouse/view.py
+++ myHouse/view.py
@@ -87,9 +87,6 @@ class View:
 					sys.stdout.write("      %s\n" % (", ".join(filter(None, text_surface.values()))))
 				
 				
-				
-				
-				
 				hist = collections.OrderedDict()
 				hist['location'] = 						self.printRow_extraData('location', extr, 'location', 			'in %s', 'location')
 				hist['year_built'] = 					self.printRow_extraData('year', 	extr, 'year_built', 		'constr in: %d', 'year')
