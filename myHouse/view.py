752dea74fe87f014842c21417641f694c624e3df
Lucian Sirbu <quamis@gmail.com>
2013-01-17 20:31:36 +0200

    Revert "FIX: code cleanup"
    This reverts commit 94f1ae9bbf9b987784099b6f2bdd4028f57855b1.

--- myHouse/view.py
+++ myHouse/view.py
@@ -93,6 +93,9 @@ class View:
 				
 				
 				
+				
+				
+				
 				hist = collections.OrderedDict()
 				hist['location'] = 						self.printRow_extraData('location', extr, 'location', 			'in %s', 'location')
 				hist['year_built'] = 					self.printRow_extraData('year', 	extr, 'year_built', 		'constr in: %d', 'year')
