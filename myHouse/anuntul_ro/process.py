ff4d92a29f0bba8198b1d670679a270af435d990
Lucian Sirbu <quamis@gmail.com>
2013-01-19 14:54:03 +0200

    FIX: refactored anuntul_ro and moved some common funcitonality into a base class, so we'd use it in other "text-based" parsers

--- myHouse/anuntul_ro/process.py
+++ myHouse/anuntul_ro/process.py
@@ -4,43 +4,6 @@ import re
 class doProcess(base.process.Processor ):
     def __init__(self, source, maindb, db, cache, args):
         super(doProcess, self).__init__(source, maindb, db, cache, args)
-        
-        self.reformatter = {
-                 'replace:content': {
-                    "[\s]*-[\s]*" : " - ",
-                    "[^a-z](facultate|facultatea)[^a-z]*" : " Facultate ",
-                    "[^a-z](sos\.)[^a-z]*" : " sos. ",
-                    "[^a-z](metrou)[^a-z]*" : " ",
-                 },
-                 'cleanup:tail': (
-                    "ocazie", "urgent", "deosebit(a)", "lux", "ieftin(a)?",
-                    "(constr|construcita|contruit)([\s]*|\.|\,)[0-9]+",
-                    "(supr|supraf|suprafata)", "mp", "[0-9]+[\s]*m", "[0-9]+[\s]*mp", "[0-9]+[\s]*m2",
-                    "de", "in", "la", "cu", "si", "are",
-                    "teren(ul)?", "zona", "cheie",
-                    "metrou", "stati(a|e)",
-                    "vil(a|e)", "cas(a|e)", 
-                    "gars", "garsonier(a|e)", 
-                    "apt", "apart", "apartament(e)?",
-                    "duplex",
-                    "(vand|vanzare)", "schimb", "zona", "foarte", "linistit(a)?", 
-                    "central", "ultracentral(a)?", "central(a)?",
-                    "caramida", "bca", "paianta", "pamant",    
-                    "bucatarie", "living", "dormitor", "dormitoare",
-                    "utilat(a)?", "mobilat(a)?",   
-                    "[0-9]+ (cam|camera|camere)(\.)?", 
-                    "oras", "sector [0-9]", "intrare",  
-                    "istorie", "istoric(a)?", "valoare",  
-                    "arhitectura", "arhitecturala", 
-                    "prop(r)?ietar(\.)?", "particular",
-                    "acces", "toate", "actele", "locuibil(a)?", "demolabil(a)?", "facilitati(le)?",  
-                    "strada", "stradal(a)?", "asfaltat(a)?", "pta", "piata", "p-ta", 
-                    "scoala", "gradinita", "cablu", "tv", "telefon", "curent(a)?", "apa", "canalizare", "gaz", "utilitati(le)?",
-                    "nr(\.)? ([0-9]+[A-Z])?",
-                    "[0-9\.]+ km de (.+)", 
-                    "-", "[\s]", "\."
-                    ) 
-                 }
     
     def selectStart(self):
         c = self.db.selectStart("SELECT `category`, `contact`, `description`, `url`, `price`, `id`  FROM `anuntul_ro_data` WHERE 1 ORDER BY `category` ASC, `description` ASC")
@@ -49,126 +12,36 @@ class doProcess(base.process.Processor ):
     def selectEnd(self, c):
         self.db.selectEnd(c)
         
-    def reformat(self, text, formulas = { }):
-        tx = ""
-        
-        if "cleanup:tail" in formulas:
-            while tx!=text:
-                tx = text
-                text = re.sub("([\s]|^)("+"|".join(formulas['cleanup:tail'])+")[\s]*$", "", tx, 0, re.IGNORECASE)
-                
-        if "replace:content" in formulas:
-            for k in formulas["replace:content"].iterkeys():
-                text = re.sub(k, formulas["replace:content"][k], text, 0, re.IGNORECASE)
-                
-        return text
-    
     def _extractData_houses(self, newRow):
         extr = {}
+        #desc = re.sub("\xa0|\d20ac", "", newRow['description'])
         desc = newRow['description']
         
-        m = re.search("^(?P<location>([A-Za-z0-9-\.]+( |(?=,)))+)", desc)
-        if m:
-            extr['location']  = m.group('location')
-            t = self.reformat(m.group('location'), self.reformatter)
-            if t:
-                extr['location'] = t
-            
-        m = re.search("(?P<rooms>[0-9]+)[\s](dormitoare|camere|cam\.|cam)", desc)
-        if m:
-            extr['rooms'] = m.group('rooms')
-            
-
-        # extract surface_built
-        if "surface_built" not in extr:        
-            m = re.search("(utili|amprenta|casa|casa de|suprafata (utila|casei|casa)|supr(\.)? (utila|casei|casa)|utila|su|s\.u\|suprafata( de)?.|construita( de)?.)[\s]?(?P<supr>[0-9]+)[\s]?(mpu|mp|m\.p\.)", desc)
-            if m:
-                extr['surface_built'] = m.group('supr')
-                
-        if "surface_built" not in extr:        
-            m = re.search("[\s]?(?P<supr>[0-9]+)[\s]?(mpu|mpc)", desc)
-            if m:
-                extr['surface_built'] = m.group('supr')
-                
-        if "surface_built" not in extr:        
-            m = re.search("[\s]?(?P<supr>[0-9]+)[\s]?(mp|m\.p\.) (amprenta|utili|construiti|constr(\.)?)", desc)
-            if m:
-                extr['surface_built'] = m.group('supr')
-                
-                
-        # extract surface_total
-        if "surface_total" not in extr:        
-            m = re.search("(?P<supr>[0-9\.]+)[\s]?(mp|m\.p\.) (total teren|teren)", desc)
-            if m:
-                extr['surface_total'] = re.sub("(\.|\,)", "", m.group('supr'))
-                
-        if "surface_total" not in extr:        
-            m = re.search("(curte de|curte|teren|supr|suprafata)[\s]?(?P<supr>[0-9\.]+)[\s]?(mp|m\.p\.)", desc)
-            if m:
-                extr['surface_total'] = re.sub("(\.|\,)", "", m.group('supr'))
-                
-        if "surface_total" not in extr:        
-            m = re.search("(?P<supr>[0-9\.]+)[\s]?(mp|m\.p\.)", desc)
-            if m:
-                extr['surface_total'] = re.sub("(\.|\,)", "", m.group('supr'))
-                extr['surface_built'] = re.sub("(\.|\,)", "", m.group('supr'))
-                
-        # default, final search
-        if "surface_total" in extr and "surface_built" not in extr:        
-            m = re.search("(?P<supr>[0-9\.]+)[\s]?(mp|m\.p\.)", desc)
-            if m:
-                b = re.sub("(\.|\,)", "", m.group('supr'))
-                if b!=extr['surface_total']:
-                    extr['surface_built'] = b
-                     
-
-        if "surface_built" in extr and float(extr['surface_built'])>0:
+        print ""
+        print desc
+        extr = self.processor_helper.extract_location(extr, desc)
+        
+        extr = self.processor_helper.extract_rooms(extr, desc)
+        
+        extr = self.processor_helper.extract_surface(extr, desc, "case-vile")
+        
+        if newRow['price'] and "surface_built" in extr and float(extr['surface_built'])>1:
             extr['price_per_mp_built'] = round(float(newRow['price'])/float(extr['surface_built']), 0)
-            
+
         return extr
     
     def _extractData_apt(self, newRow):
         extr = {}
         desc = newRow['description']
         
-        m = re.search("^(?P<location>([A-Za-z0-9-\.]+( |(?=,)))+)", desc)
-        if m:
-            extr['location']  = m.group('location')
+        print ""
+        print desc
+        extr = self.processor_helper.extract_location(extr, desc)
+        
+        extr = self.processor_helper.extract_rooms(extr, desc)
             
-        m = re.search("(?P<rooms>[0-9]+)[\s](dormitoare|camere|cam\.|cam)", desc)
-        if m:
-            extr['rooms'] = m.group('rooms')
-            
+        extr = self.processor_helper.extract_surface(extr, desc, "apt")
 
-        # extract surface_built
-        if "surface_built" not in extr:        
-            m = re.search("(utili|suprafata( de)?.|construita( de)?.)[\s]?(?P<supr>[0-9]+)[\s]?(mpu|mp|m\.p\.)", desc)
-            if m:
-                extr['surface_built'] = m.group('supr')
-                
-        if "surface_built" not in extr:        
-            m = re.search("[\s]?(?P<supr>[0-9]+)[\s]?(mpu|mpc)", desc)
-            if m:
-                extr['surface_built'] = m.group('supr')
-                
-        if "surface_built" not in extr:        
-            m = re.search("[\s]?(?P<supr>[0-9]+)[\s]?(mp|m\.p\.) (|utili|construiti|constr(\.)?)", desc)
-            if m:
-                extr['surface_built'] = m.group('supr')
-                
-                
-        # extract surface_total
-        if "surface_total" not in extr:        
-            m = re.search("(?P<supr>[0-9\.]+)[\s]?(mp|m\.p\.) (total)", desc)
-            if m:
-                extr['surface_total'] = re.sub("(\.|\,)", "", m.group('supr'))
-                
-        if "surface_total" not in extr:        
-            m = re.search("(?P<supr>[0-9\.]+)[\s]?(mp|m\.p\.)", desc)
-            if m:
-                extr['surface_total'] = re.sub("(\.|\,)", "", m.group('supr'))
-                extr['surface_built'] = re.sub("(\.|\,)", "", m.group('supr'))
-                
         if "surface_built" in extr and float(extr['surface_built'])>0:
             extr['price_per_mp_built'] = round(float(newRow['price'])/float(extr['surface_built']), 0)
             
@@ -192,15 +65,9 @@ class doProcess(base.process.Processor ):
         # extract data
         if newRow['category']=="case-vile":
             newRow['extracted'] = self._extractData_houses(newRow)
+            pass
         else:
             newRow['extracted'] = self._extractData_apt(newRow)
         
-                
-            #raise SystemExit  
-        
-        #raise SystemExit
-        
-        
-        #print newRow
         return newRow
     
--- myHouse/base/process.py
+++ myHouse/base/process.py
@@ -1,5 +1,4 @@
-import time
-import sys
+import time, sys, re
 
 class Processor(object):
     def __init__(self, source, maindb, db, cache, args):
@@ -8,6 +7,8 @@ class Processor(object):
         self.db = db
         self.maindb = maindb
         self.cache = cache
+        
+        self.processor_helper = Processor_helper()
 
         #email, phones        
         self.maindb.tableCreate("data", { 
@@ -137,3 +138,314 @@ class Processor(object):
             
         self.selectEnd(rows)
         self.maindb.close()
+
+
+
+
+class Processor_helper(object):
+    def __init__(self):
+        self.presets = {}
+        self.presets['case-vile'] = {}
+        self.presets['case-vile']['reformatter'] = {}
+        self.presets['case-vile']['reformatter']['extractLocation'] = {
+                 'replace:content': {
+                    "[\s]*-[\s]*" : " - ",
+                    "[^a-z](facultate|facultatea)[^a-z]*" : " Facultate ",
+                    "[^a-z](sos\.)[^a-z]*" : " sos. ",
+                    "[^a-z](metrou)[^a-z]*" : " ",
+                 },
+                 'cleanup:tail': (
+                    "ocazie", "urgent", "deosebit(a)", "lux", "ieftin(a)?",
+                    "(constr|construcita|contruit)([\s]*|\.|\,)[0-9]+",
+                    "(supr|supraf|suprafata)", "mp", "[0-9]+[\s]*m", "[0-9]+[\s]*mp", "[0-9]+[\s]*m2",
+                    "de", "in", "la", "cu", "si", "are",
+                    "teren(ul)?", "zona", "cheie",
+                    "metrou", "stati(a|e)",
+                    "vil(a|e)", "cas(a|e)", 
+                    "gars", "garsonier(a|e)", 
+                    "apt", "apart", "apartament(e)?",
+                    "duplex",
+                    "(vand|vanzare)", "schimb", "zona", "foarte", "linistit(a)?", 
+                    "central", "ultracentral(a)?", "central(a)?",
+                    "caramida", "bca", "paianta", "pamant",    
+                    "bucatarie", "living", "dormitor", "dormitoare",
+                    "utilat(a)?", "mobilat(a)?",   
+                    "[0-9]+ (cam|camera|camere)(\.)?", 
+                    "oras", "sector [0-9]", "intrare",  
+                    "istorie", "istoric(a)?", "valoare",  
+                    "arhitectura", "arhitecturala", 
+                    "prop(r)?ietar(\.)?", "particular",
+                    "acces", "toate", "actele", "locuibil(a)?", "demolabil(a)?", "facilitati(le)?",  
+                    "strada", "stradal(a)?", "asfaltat(a)?", "pta", "piata", "p-ta", 
+                    "scoala", "gradinita", "cablu", "tv", "telefon", "curent(a)?", "apa", "canalizare", "gaz", "utilitati(le)?",
+                    "nr(\.)? ([0-9]+[A-Z])?",
+                    "[0-9\.]+ km de (.+)", 
+                    "-", "[\s]", "\."
+                    ) 
+                 }
+        
+    def reformat(self, text, formulas = { }):
+        tx = ""
+        
+        if "cleanup:tail" in formulas:
+            while tx!=text:
+                tx = text
+                text = re.sub("([\s]|^)("+"|".join(formulas['cleanup:tail'])+")[\s]*$", "", tx, 0, re.IGNORECASE)
+                
+        if "replace:content" in formulas:
+            for k in formulas["replace:content"].iterkeys():
+                text = re.sub(k, formulas["replace:content"][k], text, 0, re.IGNORECASE)
+                
+        return text
+    
+    def extract_withFormula(self, text, formulas):
+        match = None
+        m=None
+        if "match:content" in formulas:
+            rx="([\s]|^)"\
+                 "("+"|".join(formulas['match:content'][0])+")"\
+                 "[\s]*(?P<match>[0-9\.]+)[\s]*"\
+                 "("+"|".join(formulas['match:content'][1])+")"\
+                 "(\.|\,|[\s]|=|$)"
+            m = re.search(rx, text, re.IGNORECASE)
+            
+        elif "match:raw" in formulas:
+            rx=formulas["match:raw"]
+            m = re.search(rx, text, re.IGNORECASE)
+            
+
+        if m:
+            s = m.group('match')
+            
+            if s:
+                if re.search("^[0-9]+(\.|\,)[0-9]{,2}$", s):
+                    s = re.sub("(\.|\,)[0-9]{,2}$", "", s)
+                    
+                s = re.sub("(\.|\,)", "", s)
+                if s:
+                    try:
+                        if "type" in formulas:
+                            if formulas['type']=='int':
+                                match = int(s)
+                            elif formulas['type']=='float':
+                                match = int(s)
+                            elif formulas['type']=='str':
+                                match = str(s)
+                        else:
+                            match = float(s)
+                    except ValueError, e:
+                        print "Cannot convert '%s' to number: %s" % (s, e)
+                
+        return match
+    
+    def extract_location(self, extr, text):
+        s = None
+        m = re.search("^(?P<location>([A-Za-z0-9-\.]+( |(?=,)))+)", text)
+        if m:
+            extr['location']  = m.group('location')
+            s = self.reformat(extr['location'], self.presets['case-vile']['reformatter']['extractLocation'])
+            if s:
+                print "extract: location #2: %s" % (s)
+                extr['location'] = s
+            else:
+                print "extract: location #1: %s" % (s)
+        
+        if s is None:
+            print "extract: location: NOT FOUND"        
+                
+        return extr
+            
+    def extract_rooms(self, extr, text):
+        s = None    
+        if s is None:
+            s = self.extract_withFormula(text, {
+                  'type':'int',
+                  'match:raw': "(?P<match>[0-9]+)[\s]*("+"|".join([ "dormitoare", "dorm", "camere", "cam\.", "cam"])+")"
+            })
+            if s:
+                print "extract: rooms #1: %s" % (s)
+                
+        if s is None:
+            print "extract: rooms: NOT FOUND"
+        
+        if s:
+            extr['rooms'] = s
+            
+        return extr
+
+    def extract_surface(self, extr, text, profile):
+        
+        mpu = ["mpu", "mpc", "m\.p\.c\.", "m\.p\.", "mp", "m2", "metri patrati"]
+        mpt = ["m\.p\.", "mp", "m2", "metri patrati"]
+        
+        # extract surface_built
+        if profile=="case-vile":
+            s = None
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:content':((
+                          "utili|utila", 
+                          "amprenta", 
+                          "casa", 
+                          "casa de", 
+                          "suprafata (utila|casei|casa|construita|constr\.|locuibila)(:)?", 
+                          "sup(r)?(\.)? (utila|casei|casa|construita|constr\.|locuibila)(:)?", 
+                          "s(\.)?u(\.)?", 
+                          "sc(=|-|\s)?",
+                          "suprafata( de)?.", 
+                          "construita( de)?."), 
+                          mpu
+                     )})
+                if s:
+                    print "extract: surface_built #1: %s" % (s)
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:content':((
+                          "utili|utila", 
+                          "amprenta", 
+                          "casa", 
+                          "casa de", 
+                          "suprafata (utila|casei|casa|construita|constr\.|locuibila)(:)?", 
+                          "sup(r)?(\.)? (utila|casei|casa|construita|constr\.|locuibila)(:)?", 
+                          "s(\.)?u(\.)?", 
+                          "sc(=|-|\s)?",
+                          "suprafata( de)?.", 
+                          "(construita|constructie|constructia)( de)?."), 
+                          []
+                     )})
+                if s:
+                    print "extract: surface_built #1: %s" % (s)
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:content':((), 
+                          ( "("+ "|".join(mpu) + ")[\s]+"+
+                            "("+ "|".join(("amprenta", "utili", "construiti", "constr(\.)?")) + ")",
+                           )
+                     )})
+                if s:
+                    print "extract: surface_built #2: %s" % (s)
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:content':((), 
+                          mpu
+                     )})
+                if s:
+                    print "extract: surface_built #3: %s" % (s)
+                
+            if s is None:
+                print "extract: surface_built: NOT FOUND"
+                
+            if s:
+                extr['surface_built'] = s
+                
+                
+        elif profile=="apt":
+            s = None
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:content':((
+                          "utili|utila", 
+                          "suprafata (utila|construita|constr\.|locuibila)(:)?", 
+                          "sup(r)?(\.)? (utila|casei|casa|construita|constr\.|locuibila)(:)?", 
+                          "s(\.)?u(\.)?", 
+                          "sc(=|-|\s)?",
+                          "suprafata( de)?.", 
+                          "construita( de)?."), 
+                          mpu
+                     )})
+                if s:
+                    print "extract: surface_built #1: %s" % (s)
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:content':((), 
+                          ( "("+ "|".join(mpu) + ")[\s]+"+
+                            "("+ "|".join(("utili", "construiti", "constr(\.)?")) + ")",
+                           )
+                     )})
+                if s:
+                    print "extract: surface_built #2: %s" % (s)
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:content':((), 
+                          mpu
+                     )})
+                if s:
+                    print "extract: surface_built #3: %s" % (s)
+                
+            if s is None:
+                print "extract: surface_built: NOT FOUND"
+                
+            if s:
+                extr['surface_built'] = s
+        
+        
+        # extract surface_total
+        if profile=="case-vile":
+            s = None
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:content':((
+                            "curte de",
+                            "curte",
+                            "teren",
+                            "sup(r)?(\.)?",
+                            "suprafata",
+                            "st(=|-|\s)?",
+                            "t(=|-|\s)?",
+                            "teren(=|-|\s)?",
+                        ), 
+                          [ "|".join(mpt)]
+                     )})
+                if s:
+                    print "extract: surface_total #1: %s" % (s)
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:content':((), 
+                          ( "("+ "|".join(mpt) + "[\s]+)?"+
+                            "("+ "|".join(("total teren", "tot(\.)? teren", "de teren", "teren", "total(a)", "curte")) + ")",
+                           )
+                     )})
+                if s:
+                    print "extract: surface_total #2: %s" % (s)
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:content':((), 
+                          mpt
+                     )})
+                if s:
+                    print "extract: surface_total #3: %s" % (s)
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:raw': "(?P<match>[0-9\.]+)[\s]*("+"|".join(mpt)+")"
+                })
+                if s:
+                    print "extract: surface_total #4: %s" % (s)
+                    
+            if s is None:
+                print "extract: surface_total: NOT FOUND"
+                
+            if s:
+                extr['surface_total'] = s
+        elif profile=="apt":
+            s = None
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:content':((), 
+                          mpt
+                     )})
+                if s:
+                    print "extract: surface_total #1: %s" % (s)
+            if s is None:
+                s = self.extract_withFormula(text, {
+                      'match:raw': "(?P<match>[0-9\.]+)[\s]*("+"|".join(mpt)+")"
+                })
+                if s:
+                    print "extract: surface_total #2: %s" % (s)
+                    
+            if s is None:
+                print "extract: surface_total: NOT FOUND"
+                
+            if s:
+                extr['surface_total'] = s
+        
+        return extr
