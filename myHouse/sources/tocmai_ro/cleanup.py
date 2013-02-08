0e8078febb65df5d058446ffd6017f57ecf24b0c
Lucian Sirbu <quamis@gmail.com>
2013-02-08 22:36:30 +0200

    FIX: cleanups were'nt working

--- myHouse/sources/tocmai_ro/cleanup.py
+++ myHouse/sources/tocmai_ro/cleanup.py
@@ -1,6 +1,8 @@
-import base.cleanup
+# -*- coding: utf-8 -*-
 
-class doCleanup(base.cleanup.Cleanup ):
+import sources.base.cleanup as base
+
+class doCleanup(base.Cleanup ):
     def __init__(self, source, db, cache, args):
         super(doCleanup, self).__init__(source, db, cache, args)
         self.tables_cache = ['tocmai_ro']
