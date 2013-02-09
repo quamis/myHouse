996e9c9e561e318038c7e11107df8415057d9edd
Lucian Sirbu <quamis@gmail.com>
2013-02-10 01:06:42 +0200

    FIX: when getting statuses into the local DB, complex statuses weren't imported FIX: use complex forms of TODO/HIDE statuses when generating suggestions

--- myHouse/myHouse.sh
+++ myHouse/myHouse.sh
@@ -45,7 +45,7 @@ fi;
 
 if [ "$SYNCREMOTE" == "default" -o "$SYNCREMOTE" == "local" ]; then
     wget --quiet "$SYNC_SOURCEURL" -O "$OUTDIR/localStatuses.json"
-    STATUSES=`cat "$OUTDIR/localStatuses.json"| json_pp -f json -t json | grep -P '"([^\"]+)" : "([a-zA-Z0-9]+)"' | sed -r 's/"([^\"]+)" : "([a-zA-Z0-9]+)"(,)?/-id="\1" --newStatus="\2"/g' `
+    STATUSES=`cat "$OUTDIR/localStatuses.json"| json_pp -f json -t json | grep -P '"([^\"]+)" : "([^\"]+)"' | sed -r 's/"([^\"]+)" : "([^\"]+)"(,)?/-id="\1" --newStatus="\2"/g' `
     while read -r line; do
         if [ -n "$line" ]; then
             CMD="./view.py $line"
@@ -57,13 +57,13 @@ if [ "$SYNCREMOTE" == "default" -o "$SYNCREMOTE" == "local" ]; then
     
     
     echo "Computing suggestions lists"
-    ./view.py -category="case-vile" -status="todo" --outputFormat="id" > /tmp/TODO.json
-    ./view.py -category="case-vile" -status="hide" --outputFormat="id" > /tmp/HIDE.json
+    ./view.py -category="case-vile" -status="todo%" --outputFormat="id" > /tmp/TODO.json
+    ./view.py -category="case-vile" -status="hide%" --outputFormat="id" > /tmp/HIDE.json
     ./view.py -category="case-vile" -status=""     --outputFormat="id" > /tmp/TEST.json
     ./suggestions.py -datasetHIDE="/tmp/HIDE.json" -datasetTODO="/tmp/TODO.json" -datasetTEST="/tmp/TEST.json" -train=1 --outputFormat=json > /tmp/suggestions.json
     ./import.py -jsonFile=/tmp/suggestions.json -targetField=suggestedStatus > /tmp/import-suggestions.json
 fi;
-
+exit
 
 if [ "$SYNCREMOTE" == "default" ]; then
     ./view.py --profile=case-valide --outputFormat="html-php" >   "$OUTDIR/profile.case-valide.json"
